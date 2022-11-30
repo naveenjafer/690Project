b952aa5b7e77bd6be6edb1741909ded357596620
ankitamakker <amakker@umass.edu>
2022-11-29 22:11:32 -0500

    visualize network

--- src/main/simulation/simulator_main.py
+++ src/main/simulation/simulator_main.py
@@ -8,6 +8,8 @@ import src.main.utils.constants as consts
 
 from src.main.network.uniformNetwork import generateNetwork
 from src.main.newsArticles.articleGenerator import generateArticles
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def networkCreator():
     networkFolder = generateNetwork(
@@ -27,6 +29,17 @@ def networkCreator():
         consts.ARTICLE_COUNT,
         networkFolder
     )
+    return networkFolder, article_list
+
+def visualize_network(network_path):
+    G = nx.read_gml(network_path)
+    political_map = nx.get_node_attributes(G, "polInc")
+    colors = [consts.COLOR_MAP[political_inclination] for node, political_inclination in political_map.items()]
+    nx.draw_networkx(G, node_color=colors, with_labels=True)
+    plt.show()
+
 
 if __name__ == "__main__":
-    networkCreator()
+    networkFolder,  article_list = networkCreator()
+    # visualize graph
+    visualize_network(os.path.join(networkFolder, consts.NETWORK_ORIGINAL_FILENAME))
