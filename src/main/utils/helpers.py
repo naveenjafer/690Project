f08e135398b61562ced6b52c84009fba9a539379
ankitamakker <amakker@umass.edu>
2022-12-18 17:00:09 -0500

    rescale range

--- src/main/utils/helpers.py
+++ src/main/utils/helpers.py
@@ -1,4 +1,12 @@
+from __future__ import division
 from scipy.stats import truncnorm
+
 
 def get_truncated_normal(mean=0, sd=1, low=0, upp=10):
     return truncnorm((low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
+
+
+def rescale(n, range1, range2):
+    delta1 = range1[1] - range1[0]
+    delta2 = range2[1] - range2[0]
+    return (delta2 * (n - range1[0]) / delta1) + range2[0]
